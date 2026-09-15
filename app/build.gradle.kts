e9b1793499ab4fa6ffe533add0ed5cca7067ac77
jeremiasbritto-gif <jeremiasbritto@gmail.com>
2026-09-15 10:16:59 -0300

    Adicionar app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "br.com.karaokestudio"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "br.com.karaokestudio"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
